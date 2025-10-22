c48e14b961872b1ec4e57f4ba581d148947a1b6b
xohiraaa <hirajehangir2005@gmail.com>
2025-10-22 14:28:20 +0100

    typo fixed-triple speakers vibrating

--- sketch.js
+++ sketch.js
@@ -10,6 +10,6 @@ function draw() {
   fill(0,255,150);
   ellipse(150+ random(-3, 3), 180 + random(-3,3),60,60);
   ellipse(250 + random(-3,3), 180 + random(-3,3),80,80);
-  ellpse(200 + random(-3,3), 240 + random(-3,3),80,80);
+  ellipse(200 + random(-3,3), 240 + random(-3,3),80,80);
   
 }
