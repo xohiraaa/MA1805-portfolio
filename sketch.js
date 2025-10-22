4720bee7f0cfcc949dfc881dd27deb096205a4e1
xohiraaa <hirajehangir2005@gmail.com>
2025-10-22 13:28:26 +0100

    added small wobble so circle jitters like speaker

--- sketch.js
+++ sketch.js
@@ -4,3 +4,9 @@ function setup() {
   fill(0,255,150);
   ellipse(200,200,100,100);
 }
+
+function draw() {
+  background(10,15,20);
+  fill(0,255,150);
+  ellipse(200+ random(-5, 5), 200 + random(-5,5),100,100)
+}
