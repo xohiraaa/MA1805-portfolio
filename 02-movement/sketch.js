13737c2adcc9c8b3ad38b5cb16ca16d69815ba59
xohiraaa <hirajehangir2005@gmail.com>
2025-10-26 17:51:44 +0000

    make one circle move in loop using sin/cos
    accidentally closed the draw function- caused an error- cos wasn't defined

--- 02-movement/sketch.js
+++ 02-movement/sketch.js
@@ -1,3 +1,5 @@
+let t = 0; // time
+
 function setup() {
   createCanvas(500,500);
   noStroke();
@@ -5,5 +7,15 @@ function setup() {
 
 function draw() {
   background(10,12,20); //dark bg
-
 }
+
+// orbit maths (around 250,250 for now) 
+let angle = t;
+let x = 250 + cos(angle) * 120;
+let y =250 + sin(angle) * 120;
+
+fill(120, 255, 180); // green-ish
+ellipse(x, y, 40, 49);
+
+t += 0.02; //animation speed
+  
