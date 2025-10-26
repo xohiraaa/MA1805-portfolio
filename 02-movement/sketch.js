a6c6dd137296201d630baeba035936bf5e751d91
xohiraaa <hirajehangir2005@gmail.com>
2025-10-26 18:42:53 +0000

    change circle into ring of orbiting circles

--- 02-movement/sketch.js
+++ 02-movement/sketch.js
@@ -9,13 +9,13 @@ function draw() {
   background(10,12,20);
  push();
   translate(width / 2, height / 2); 
-
-let angle = t;
-let x = cos(angle) * 120;
-let y = sin(angle) * 120;
-
-fill(120, 255, 180); // green-ish
-ellipse(x, y, 40, 49);
+  for (let i = 0; i < 8; i++) {
+    let angle = t + i * TWO_PI / 8;
+    let x = cos(angle) * 120;
+    let y = sin(angle) * 120;
+    fill (120,255,180);
+    ellipse( x, y, 24);
+  }
 pop();
 
 t += 0.02; //animation speed
