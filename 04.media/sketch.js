23795ea970965333c461aa26ae15d11638ae5dd5
xohiraaa <hirajehangir2005@gmail.com>
2025-11-01 18:28:48 +0000

    Add files via upload

--- 04.media/sketch.js
+++ 04.media/sketch.js
@@ -1,26 +1,40 @@
-let img; // Declare variable 'img'.
-let hum;
+// Media: jungle amen-break cone pulse 
+let bass;
+let bassStart;
 
-function preload (){
-  hum = loadSound("humm.wav");
-  img = loadImage('Antenna.jpg');
+function preload() {
+  //amen break
+  bass = loadSound('244342__insidebeat__amen-break-slices-1.wav');
 }
 
 function setup() {
-  createCanvas (windowWidth, windowHeight);
+  createCanvas(400, 400);
+  background(10, 12, 20);
+  noStroke();
+  colorMode(HSB, 360, 100, 100);
 }
 
 function draw() {
-  image (img, 0, 0, windowWidth, 500);
+  background(10, 12, 20);
+
+  // only pulse after click 
+  if (bass.isPlaying()) {
+    let progress = (millis() - bassStart) / 1000; //  seconds
+    let s = map(progress, 0, bass.duration(), 20, 200);
+    fill(100 + sin(progress * 3) * 20, 80, 90); // jungle green
+    ellipse(200, 200, s, s);
+  } else {
+    // waiting text 
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text('CLICK TO START JUNGLE', 200, 200);
+  }
 }
 
-function mouseClicked() {
-  hum.loop();
-  hum.amp(0.2);
-  // IMPORTANT TO HAVE THIS!! 
-  // On many browsers,audio will only run after
-  // a user interaction (keypress, mouseclick etc)
-  if (getAudioContext(). state !== 'running') {
-  getAudioContext(). resume();
-}
+// Notion pattern: click to play + browser unblock
+function mousePressed() {
+  if (!bass.isPlaying()) {
+    bass.play();
+    bassStart = millis();
+  }
 }
